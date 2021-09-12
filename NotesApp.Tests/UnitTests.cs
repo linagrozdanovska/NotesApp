52d94857c2370c8d037dd9a8757d4d3f775387db
Гроздановска Лина <lina.grozdanovska@students.finki.ukim.mk>
2021-09-12 22:33:36 +0200

    Index method unit tests

--- NotesApp.Tests/UnitTests.cs
+++ NotesApp.Tests/UnitTests.cs
@@ -7,6 +7,7 @@ using NotesApp.Data;
 using NotesApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading;
@@ -24,16 +25,12 @@ namespace NotesApp.Tests
         {
             //Arrange
             var repo = new Mock<INoteRepository>();
-
             var store = new Mock<IUserStore<IdentityUser>>();
-
             var manager = new Mock<UserManager<IdentityUser>>(store.Object, null, null, null, null, null, null, null, null);
-
             var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
                 new Claim(ClaimTypes.NameIdentifier, "test"),
                 new Claim(ClaimTypes.Name, "test@mail.com")
             }, "TestAuthentication"));
-
             var controller = new NotesController(repo.Object, manager.Object);
             controller.ControllerContext = new ControllerContext();
             controller.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
@@ -51,13 +48,9 @@ namespace NotesApp.Tests
         {
             //Arrange
             var repo = new Mock<INoteRepository>();
-
             var store = new Mock<IUserStore<IdentityUser>>();
-
             var manager = new Mock<UserManager<IdentityUser>>(store.Object, null, null, null, null, null, null, null, null);
-
             var user = new ClaimsPrincipal(new ClaimsIdentity());
-
             var controller = new NotesController(repo.Object, manager.Object);
             controller.ControllerContext = new ControllerContext();
             controller.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
@@ -67,6 +60,101 @@ namespace NotesApp.Tests
 
             //Assert
             Assert.Equal("Home", result.ViewName);
+        }
+
+        [Theory]
+        [InlineData("")]
+        public void Index_EmptySearchString_ReturnsAllNotesFromUser(string searchstring)
+        {
+            //Arrange
+            var repo = new Mock<INoteRepository>();
+            var store = new Mock<IUserStore<IdentityUser>>();
+            var manager = new Mock<UserManager<IdentityUser>>(store.Object, null, null, null, null, null, null, null, null);
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.NameIdentifier, "test"),
+                new Claim(ClaimTypes.Name, "test@mail.com")
+            }, "TestAuthentication"));
+            repo.Setup(r => r.GetAll(user.FindFirstValue(ClaimTypes.NameIdentifier)))
+                .Returns(GetTestNotes(user.FindFirstValue(ClaimTypes.NameIdentifier)));
+            var controller = new NotesController(repo.Object, manager.Object);
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
+
+            //Act
+            var result = controller.Index(searchstring);
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var notes = Assert.IsType<List<Note>>(viewResult.Model);
+            Assert.Equal(3, notes.Count);
+        }
+
+        [Theory]
+        [InlineData(":)")]
+        public void Index_SearchString_ReturnsNotesContainingSearchStringFromUser(string searchstring)
+        {
+            //Arrange
+            var repo = new Mock<INoteRepository>();
+            var store = new Mock<IUserStore<IdentityUser>>();
+            var manager = new Mock<UserManager<IdentityUser>>(store.Object, null, null, null, null, null, null, null, null);
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.NameIdentifier, "test"),
+                new Claim(ClaimTypes.Name, "test@mail.com")
+            }, "TestAuthentication"));
+            repo.Setup(r => r.GetAll(user.FindFirstValue(ClaimTypes.NameIdentifier)))
+                .Returns(GetTestNotes(user.FindFirstValue(ClaimTypes.NameIdentifier)));
+            var controller = new NotesController(repo.Object, manager.Object);
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
+
+            //Act
+            var result = controller.Index(searchstring);
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var notes = Assert.IsType<List<Note>>(viewResult.Model);
+            Assert.Equal(2, notes.Count);
+            Assert.All(notes, item => Assert.True(item.Title.Contains(searchstring) || item.Body.Contains(searchstring)));
+        }
+
+        private List<Note> GetTestNotes(string userId)
+        {
+            List<Note> output = new List<Note>
+            {
+                new Note
+                {
+                    Id = 0,
+                    UserId = "test",
+                    Date = DateTime.Now,
+                    Title = "Test Note 1",
+                    Body = "Body of note 1."
+                },
+                new Note
+                {
+                    Id = 1,
+                    UserId = "diff",
+                    Date = DateTime.Now,
+                    Title = "Note 1",
+                    Body = "Lorem ipsum dolor sit amet."
+                },
+                new Note
+                {
+                    Id = 2,
+                    UserId = "test",
+                    Date = DateTime.Now,
+                    Title = "Test Note 2",
+                    Body = "Hello :)."
+                },
+                new Note
+                {
+                    Id = 3,
+                    UserId = "test",
+                    Date = DateTime.Now,
+                    Title = "Test Note 3",
+                    Body = "This is note 3 :)."
+                }
+            };
+            return output.Where(z => z.UserId.Equals(userId)).ToList();
         }
     }
 }
